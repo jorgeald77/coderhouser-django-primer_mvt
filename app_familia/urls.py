e4b67ada7e1d8aff2c911cd50ed29d75137a4a96
Jorge G <jorgeald77@gmail.com>
2022-05-24 18:34:07 -0500

    Se da funcionalidad a los links de la vista index.html de app_familia

--- app_familia/urls.py
+++ app_familia/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from app_familia import views
 
 urlpatterns = [
-    path('', views.index),
-    path('show/<int:id>', views.show)
+    path('', views.index, name='index'),
+    path('show/<int:id>', views.show, name='show'),
 ]
